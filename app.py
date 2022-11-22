163a20e1a6b41b55fc93e544647703bf115af66a
SamLizard <90714263+SamLizard@users.noreply.github.com>
2022-11-22 13:35:19 +0200

    tutorial 7

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "PikaoVerde"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'slite://users/sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFCATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class users(db.model)
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name=name
+        self.email=email
 
 @app.route("/")
 def home():
@@ -19,11 +33,20 @@ def login():
             return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/user/")
+@app.route("/user/" , methods=["POST", "GET"])
 def user():
+    email=None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html", user=user)
+
+        if request.method == "POST":
+            email= request.form["email"]
+            session["email"] =email
+            flash ("Email saved!", "info")
+        else:
+            if "email" in session:
+                email= session["email"]
+        return render_template("user.html", email=email)
     else:
         return redirect(url_for("login"))
 
@@ -32,7 +55,9 @@ def logout():
     if "user" in session:
         flash("Logged out successfully!", "info")
     session.pop("user", None)
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
